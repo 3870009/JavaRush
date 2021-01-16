e63e9a279a2d73fd48b42b9c588d64aef2f4e11b
RomanTopcheev <3870009@gmail.com>
2021-01-16 18:53:33 +0300

    Initial commit

---
+++ src/eu/senla/task1/Main.java
@@ -0,0 +1,18 @@
+package eu.senla.task1;
+
+public class Main {
+    static int d;
+
+    public static void main(String[] args) {
+       Number number = new Number();
+       number.setA((byte)12);
+        int s = 12;
+        byte b = 17;
+        Integer i = 10;
+        Integer in = new Integer(10);
+        byte f = (byte) s;
+
+        System.out.println(number.getA());
+        System.out.println(f);
+    }
+}
---
+++ src/eu/senla/task1/Number.java
@@ -0,0 +1,16 @@
+package eu.senla.task1;
+
+public class Number {
+    byte a;
+    short b;
+    int c;
+    long d;
+
+    public byte getA() {
+        return a;
+    }
+
+    public void setA(byte a) {
+        this.a = a;
+    }
+}
